88107aad6fe7cfb6e9e5596ab55cc01d3e11f234
Alim Siemanym <alimsiemanym@gmail.com>
2011-09-23 19:31:44 -0600

    S000 - first commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.10'
+gem 'haml'
+
+group :development, :test do
+  gem 'heroku'
+  gem 'sqlite3'
+  gem 'heroku_san'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'faker'
+  gem 'rcov'
+  gem 'annotate'
+  gem 'mocha'
+  gem 'haml_scaffold'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenBeta::Application.config.secret_token = '8a4f16e4a6c56228ead22d34de484c2156741abe8f6d08bb0875ca65382b85871636e9c98cb2f554a8be10f688bedb54cd7d10d249274b2788752b7ee697492a'
