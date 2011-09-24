8c02cc4b4158a164d5f91680b19a880c98f2fa11
Alim Siemanym <alimsiemanym@gmail.com>
2011-09-24 00:01:30 -0600

    S002 seed data for genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+# Load external seeds
+require File.expand_path('../seeds/genres', __FILE__) 
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
